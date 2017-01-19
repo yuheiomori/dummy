ebd094b4b1337e80a48f7b61fd0c1c45a685c82e
Yuhei Omori <yuhei.omori@beproud.jp>
2017-01-19 15:06:14 +0900

    fix slash

--- src/app/urls.py
+++ src/app/urls.py
@@ -9,12 +9,12 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r"^$", index, name="index"),
-    url(r"^namahage?/$", namahage, name="namahage"),
+    url(r"^namahage/?$", namahage, name="namahage"),
     url(r"^ogp(?P<original_path>.*)$", ogp, name="ogp"),
-    url(r"^namahage2?/$", namahage, name="namahage2"),
-    url(r"^namahage3?/$", namahage, name="namahage3"),
-    url(r"^bounce?/$", bounce, name="bounce"),
-    url(r"^complaint?/$", complaint, name="complaint"),
+    url(r"^namahage2/?$", namahage, name="namahage2"),
+    url(r"^namahage3/?$", namahage, name="namahage3"),
+    url(r"^bounce/?$", bounce, name="bounce"),
+    url(r"^complaint/?$", complaint, name="complaint"),
 
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve'),
 
