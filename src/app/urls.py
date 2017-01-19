3dc96135d6c8dd35e0fcc44ff5804ee3110ee588
Yuhei Omori <yuhei.omori@beproud.jp>
2017-01-19 14:47:24 +0900

    add views

--- src/app/urls.py
+++ src/app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 
-from app.users.views import index, namahage, ogp
+from app.users.views import index, namahage, ogp, bounce, complaint
 
 admin.autodiscover()
 
@@ -13,5 +13,9 @@ urlpatterns = patterns(
     url(r"^ogp(?P<original_path>.*)$", ogp, name="ogp"),
     url(r"^namahage2?/$", namahage, name="namahage2"),
     url(r"^namahage3?/$", namahage, name="namahage3"),
+    url(r"^bounce?/$", bounce, name="bounce"),
+    url(r"^complaint?/$", complaint, name="complaint"),
+
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve'),
+
 )
--- src/app/users/views.py
+++ src/app/users/views.py
@@ -20,3 +20,20 @@ def ogp(request, original_path):
     logger.debug(request.GET)
     logger.debug(original_path)
     return TemplateResponse(request, 'ogp.html', {})
+
+
+def bounce(request):
+    logger.debug('bounce')
+    logger.debug(request.META)
+    logger.debug(request.GET)
+    logger.debug(request.POST)
+
+    return TemplateResponse(request, 'bounce.html', {})
+
+
+def complaint(request):
+    logger.debug('complaint')
+    logger.debug(request.META)
+    logger.debug(request.GET)
+    logger.debug(request.POST)
+    return TemplateResponse(request, 'complaint.html', {})
